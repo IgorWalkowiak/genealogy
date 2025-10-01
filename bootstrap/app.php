c2f692f6ab06b27b5cbd0d9bcb41a187e3cefc52
Igor <igorwalkowiak@protonmail.com>
2025-10-01 13:26:10 +0200

    CSRF off

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            '*',
+        ]);
+        
         $middleware->web(append : [
             App\Http\Middleware\Localization::class,
 
