2fe532194e36fc5012b47dd34966dec0c44cf5cd
Igor <igorwalkowiak@protonmail.com>
2025-10-02 22:20:01 +0200

    stoarge

--- config/filesystems.php
+++ config/filesystems.php
@@ -109,7 +109,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage') => env('STORAGE_PUBLIC_PATH', storage_path('app/public')),
     ],
 
 ];
